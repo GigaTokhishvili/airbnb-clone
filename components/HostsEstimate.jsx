ad3b280c14298fea3a7ce1807ee0724694f56362
GigaTokhishvili <toxishvili123@gmail.com>
2023-11-23 19:11:28 +0400

    testing geolocation2

--- components/HostsEstimate.jsx
+++ components/HostsEstimate.jsx
@@ -14,19 +14,13 @@ function HostsEstimate({ offEstimate }) {
                 <h1 className='font-semibold mb-5 sm:text-3xl'>How we estimate your earning potential</h1>
             </div>
             <div className='px-6 leading-5'>
-                <p>To estimate your earnings, we review the past 12 months of booking data from similar Airbnb listings. We choose these listings based on the information you share about your place. If you enter an address, you'll get a more specific estimate based on the listings closest to you. If you enter an area, we look at the top 50% of similar listings in that area, based on their earnings.</p>
+                <p>To estimate your earnings, we review the past 12 months of booking data from similar Airbnb listings. We choose these listings based on the information you share about your place. If you enter an address, you&apos;ll get a more specific estimate based on the listings closest to you. If you enter an area, we look at the top 50% of similar listings in that area, based on their earnings.</p>
 
-                <p className='pt-5'>
-                    Based on these similar listings, we estimate the average nightly earnings and multiply that number by the number of nights you indicate you will host. We also provide the average number of nights booked per month in your area, assuming places are available on Airbnb every night of the month. (Nightly earnings are the price set by each Host minus the <a className='text-black font-semibold underline' href="https://www.airbnb.co.uk/help/article/1857" target='_blank'>Airbnb Host Service fee</a>. We don't subtract taxes or hosting expenses.)
-                </p>
+                <p className='pt-5'>Based on these similar listings, we estimate the average nightly earnings and multiply that number by the number of nights you indicate you will host. We also provide the average number of nights booked per month in your area, assuming places are available on Airbnb every night of the month. (Nightly earnings are the price set by each Host minus the <a className='text-black font-semibold underline' href="https://www.airbnb.co.uk/help/article/1857" target='_blank'>Airbnb Host Service fee</a>. We don&apos;t subtract taxes or hosting expenses.)</p>
 
-                <p className='pt-5'>
-                    Your actual earnings will depend on several factors, including your availability, price and the demand in your area. Your ability to host may also depend on local laws. Learn more about <a className='text-black font-semibold underline' href="https://www.airbnb.co.uk/help/article/376" target='_blank'>responsible hosting</a>.
-                </p>
+                <p className='pt-5'>Your actual earnings will depend on several factors, including your availability, price and the demand in your area. Your ability to host may also depend on local laws. Learn more about <a className='text-black font-semibold underline' href="https://www.airbnb.co.uk/help/article/376" target='_blank'>responsible hosting</a>.</p>
 
-                <p className='pt-5 pb-10'>
-                    These earning estimates are not an appraisal or estimate of property value.
-                </p>
+                <p className='pt-5 pb-10'>These earning estimates are not an appraisal or estimate of property value.</p>
             </div>
         </div>
     </div>
